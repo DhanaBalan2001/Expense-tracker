e9535cf3213205150d16c3fe0ad18eece55ea32b
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-11-13 14:37:26 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -32,7 +32,9 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors({
     origin: [ 'http://localhost:5173', 'https://expense-tracker-dhanabalan.netlify.app'],
-    credentials: true
+    credentials: true ,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
