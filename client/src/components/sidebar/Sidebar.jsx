d79a49adeb4d55aae4d29d7efa908c22ea8b6852
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-11-13 14:21:05 +0530

    Initial commit

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
-import './Sidebar.css';
+import './sidebar.css';
 
 const Sidebar = () => {
   const location = useLocation();
